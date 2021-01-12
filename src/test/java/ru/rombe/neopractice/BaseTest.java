764fd96e3a1e924a2bfa303440aecc51809d2712
Belinsky Roman <romanbelinsky99@gmail.com>
2021-01-13 02:16:20 +0600

    Add class BaseTest simple testing

---
+++ src/test/java/ru/rombe/neopractice/BaseTest.java
@@ -0,0 +1,34 @@
+package ru.rombe.neopractice;
+
+import org.junit.jupiter.api.Assertions;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public abstract class BaseTest {
+    public static void createFile(String filename) {
+        try {
+            Files.createFile(Paths.get(filename));
+        } catch (IOException e) {
+            Assertions.fail();
+        }
+    }
+
+    public static void deleteFile(String filename) {
+        try {
+            Files.deleteIfExists(Paths.get(filename));
+        } catch (IOException e) {
+            Assertions.fail();
+        }
+    }
+
+    public static void writeToFile(String filename, String data) {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(filename))) {
+            writer.write(data);
+        } catch (IOException e) {
+            Assertions.fail();
+        }
+    }
+}
